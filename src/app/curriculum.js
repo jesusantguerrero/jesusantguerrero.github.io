d4b531ffa9b3a5ead7d28bbb6e3e7d5865d54581
Jesus Guerrero <jesusant.guerrero@gmail.com>
2017-11-09 00:01:01 -0400

    base

---
+++ src/app/curriculum.js
@@ -0,0 +1,39 @@
+const about = {
+  description: '',
+  image: ''
+}
+
+const skills = {
+  currently: [
+    {name: '', description: ''}
+  ],
+
+  technologies: [
+    {name: '',level: 1}
+  ]
+}
+
+const portfolio = [
+  {title: '', technologies: '', description: '', image: '', link: ''}
+]
+
+const contacts = {
+  message: '',
+  networks: [
+    {name: '', link:''}
+  ]
+}
+
+// my blog, my youtube channel, the company
+const work = {
+
+}
+
+
+export default curriculum = {
+  about,
+  skills,
+  portfolio,
+  work,
+  contacts
+}
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+module.exports = {
+  entry: ['./src/app/index.js'],
+  output: 'bundle.js',
+  path: path.resolve(__dirname, 'dist')
+}
