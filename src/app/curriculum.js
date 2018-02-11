859ed8b943180b24c6663727ff01ead02a088077
jesusantguerrero <me@jesusantguerrero.com>
2018-02-11 01:54:48 -0400

    :sparkles: add backend projects

--- src/app/curriculum.js
+++ src/app/curriculum.js
@@ -18,12 +18,12 @@ const portfolio = [
     title: 'Phone Calculator', 
     technologies: ['javascript', 'vue.js', 'design-patterns', 'sass', 'pug'],
     description: 'It is an android calculator clone, it has order precedence and cool animations',
-    image: 'https://s3-us-west-2.amazonaws.com/i.cdpn.io/1159667.JOXGrW.small.34799fba-8f53-41af-aa06-0677458cccf9.png', 
+    image: './assets/img/calculator.png', 
     link: 'https://codepen.io/freesgen/pen/JOXGrW', 
     type: 'frontend'
   },
   { 
-    title: 'Wikipedia Viewer',
+    title: 'Wiki-App',
     technologies: ['javascript', 'vue.js', 'sass', 'wikipedia API','AJAX'],
     description: 'It is a searcher for wikipedia articles using their API ',
     image: './assets/img/wikiapp.png', 
@@ -31,10 +31,10 @@ const portfolio = [
     type: 'frontend'
   },
   { 
-    title: 'Simon Game', 
-    technologies: ['javascript', 'vue.js', 'sass', 'custom AI'],
-    description: 'The clasic simon game in the web and programmer version',
-    image: './assets/img/simon.png', 
+    title: 'Promodoro-clock', 
+    technologies: ['javascript', 'vue.js', 'sass', 'pug'],
+    description: 'timer that allow users to follow the promodoro technique',
+    image: './assets/img/promodoro-timer.png', 
     link: 'https://codepen.io/freesgen/pen/EbwvXK?editors=0110', 
     type: 'frontend'
   },
@@ -67,11 +67,27 @@ const portfolio = [
   },
 
   { 
-    title: 'IC Services Concept', 
+    title: 'Stock-Market', 
+    technologies: ['React' , 'node.js/express',  'mongoDB', 'API`s'],
+    description: 'Application that provide stock market data and allow users to add and remove stocks',
+    image: './assets/img/stock-market.png', 
+    link: 'https://ic-stock-market.herokuapp.com/', 
+    type: 'website'
+  },
+  { 
+    title: 'ICS Web Concept', 
     technologies: ['Responsive' , 'HTML/CSS/SASS',  'Vue.js'],
     description: 'A concept of website for Island Communication Services',
     image: './assets/img/ics-concept-page.png', 
     link: 'https://jesusantguerrero.com/ics-concept-page/', 
+    type: 'website'
+  },
+  { 
+    title: 'Nightlife', 
+    technologies: ['React' , 'Node.js/express',  'MongoDB', 'Yelp-Api'],
+    description: 'Look for bars near to you, add yourself to the attendence list and se how many people are going',
+    image: './assets/img/nightlife.png', 
+    link: 'https://ic-nightlife.herokuapp.com//', 
     type: 'website'
   },
 ]
